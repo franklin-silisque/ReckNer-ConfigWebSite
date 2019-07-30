db5af1c65c02387347add0113517a2181e0eaf71
franklin <franksdb@gmail.com>
2019-07-30 19:20:15 -0400

    Adding Code for reckner project

--- ReckNer-ConfigWebSite/Program.cs
+++ ReckNer-ConfigWebSite/Program.cs
@@ -16,7 +16,7 @@ namespace ReckNer_ConfigWebSite
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmConfig());
         }
     }
 }
